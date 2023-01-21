a00d92ff440835fd19d8c2a76ec1bd77a46f402d
saifion33 <mxofficialplayer@gmail.com>
2023-01-21 12:11:29 +0530

    replace useState with useReducer in show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,29 +1,50 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchData } from '../utils/util_Snippets'
 import loadingSpinner from '../assets/Iphone-Spinner-2.gif'
+
 // import ErrorPage from '../error/ErrorPage'
 
 const Show = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [results, setResults] = useState(null)
+
   const { id } = useParams()
+
+  const initialState = {
+    isLoading: true,
+    error: null,
+    results: null
+  }
+  const reducer = (prevState, action) => {
+    switch (action.type) {
+      case 'FETCH_SUCCESS':
+        return { isLoading: false, error: null, results: action.show }
+      case 'FETCH_FAILED':
+        return { ...prevState, isLoading: false, error: action.error }
+      default:
+        return prevState
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
   useEffect(() => {
-    fetchData(`shows/${id}?embed[]=seasons&embed[]=cast`).then(result => setResults(result)).catch(err => setError(err.message)).finally(setIsLoading(false))
-  }, [])
+    fetchData(`shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(
+        results => { dispatch({ type: 'FETCH_SUCCESS', show: results }); console.log(results) }
+      )
+      .catch(
+        err => { dispatch({ type: 'FETCH_FAILED', error: err }) })
+  }, [id])
   return (
     <div>
       {
-        isLoading && <div className='flex justify-center'><img src={loadingSpinner} alt="loading spinner" /></div>
+        state.isLoading && <div className='flex justify-center'><img src={loadingSpinner} alt="loading spinner" /></div>
 
       }
       {
-        error && <div>There is an error</div>
+        state.error && <div>There is an error</div>
       }
       {
-
+        state.results && <div>open console</div>
       }
 
     </div>
